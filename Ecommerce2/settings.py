dfc7bef03fc2370f022553e23e2b3aacb158d737
ABID HUSSAIN SIDDHPURA <abidhussainsiddhpura2703@gmail.com>
2024-09-18 18:13:52 +0530

    initial commit

--- Ecommerce2/settings.py
+++ Ecommerce2/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +144,8 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 STATICFIES_DIR = {
     os.path.join(BASE_DIR,'public/static')
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR ,"public/static")
 MEDIA_URL = '/media/'
--- Ecommerce2/settings_rail.py
+++
@@ -1,5 +0,0 @@
-from Ecommerce2.settings import *
-from decouple import config
-import os
-
-SECRET_KEY = config('SECRET_KEY')
