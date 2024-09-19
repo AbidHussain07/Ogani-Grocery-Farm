9aaaa36b3ba9efc789bbb0b720a8b8756d3a0571
ABID HUSSAIN SIDDHPURA <abidhussainsiddhpura2703@gmail.com>
2024-09-19 16:47:53 +0530

    Added Wishlist Count

--- Ecommerce2/settings.py
+++ Ecommerce2/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR , 'Templates')
 SECRET_KEY = 'django-insecure-ebj9id^40&&!2fr7zven_mwm!3&!9fhp6kt$d!n@1ci8b+%25h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["ogani-fresh-farm.onrender.com","127.0.0.1","localhost"]
 
